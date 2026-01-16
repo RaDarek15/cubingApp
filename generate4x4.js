4afe1d17a68618159375d3c268d82d387640d853
Vaxu44 <igor.marczak44@gmail.com>
2026-01-16 09:06:58 +0100

    4x4

---
+++ generate4x4.js
@@ -0,0 +1,31 @@
+function generate4x4() {
+    const moves = ["U", "D", "L", "R", "F", "B"];
+    const modifiers = ["", "'", "2","w","w2"];
+    const axisMap = { U: "UD", D: "UD", L: "LR", R: "LR", F: "FB", B: "FB" };
+
+    let zakresRuchowy = Math.floor((Math.random() * 60) + 40);
+
+    const scramble = [];
+    let lastMove = "";
+    let lastAxis = "";
+    let secondLastAxis = "";
+
+    while (scramble.length < zakresRuchowy) {
+        const move = moves[Math.floor(Math.random() * moves.length)];
+        const modifier = modifiers[Math.floor(Math.random() * modifiers.length)];
+        const fullMove = move + modifier;
+        const axis = axisMap[move];
+
+        if (move === lastMove) continue;
+
+        if (axis === lastAxis && axis === secondLastAxis) continue;
+
+        scramble.push(fullMove);
+
+        secondLastAxis = lastAxis;
+        lastAxis = axis;
+        lastMove = move;
+    }
+
+    return scramble;
+}
